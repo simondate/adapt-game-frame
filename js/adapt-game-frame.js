a772e13d319f2b186e18d4889395d33b7b76578e
Simon Date <simon@adaptfreelancer.com>
2020-07-14 22:09:08 +0100

    Updates to code

--- js/adapt-game-frame.js
+++ js/adapt-game-frame.js
@@ -3,53 +3,36 @@
 * License - http://github.com/adaptlearning/adapt_framework/LICENSE
 * Maintainers - Kristin Anthony (kristin@knanthony.com)
 */
-define(function(require) {
-
-    var ComponentView = require("coreViews/componentView");
-    var Adapt = require("coreJS/adapt");
-    var fired = false;
-
+define([
+  'core/js/adapt',
+  'core/js/views/componentView',
+  'core/js/models/componentModel'
+], function(Adapt, ComponentView, ComponentModel) {
+    var visited = false;
 
     var GameIframe = ComponentView.extend({
         events: {
             'inview':'inview'
         },
 
-        preRender: function() {
-        },
-
         postRender: function() {
             var that = this;
-            /*this.$('.gameIframe-iframe').ready(function() {
-                that.setReadyStatus();
-            });*/
-            this.$('.gameIframe-object').ready(function() {
+            this.$('.gameIframe-iframe').ready(function() {
                 that.setReadyStatus();
             });
         },
-        /* This function only allows the data or src of the game frame to be set once */
-        once: function(fired) {
-          if (!fired) {
-            /* Options included to allow for using either the iframe or the object tag */
-            this.$('.gameIframe-object').attr('data', this.model.get('_source'));
-            this.model.set('_isFired', true);
-            // this.$('.gameIframe-iframe').attr('src', this.model.get('_source'));
-          } else {
-            return;
-          }
-        },
 
         gameComplete: function(e) {
-          if (e.origin === this.model.get('_originURL')) {
+          console.log(e.data)
+          console.log(this.model)
+          if (e.data === this.model.get('_id')) {
             this.setCompletionStatus();
           }
         },
 
         inview: function(event, visible) {
-            if (visible) {
-              fired = this.model.get('_isFired');
-              this.once(fired);
-              //fired = true;
+            if (visible && !visited) {
+              visited = true;
               window.addEventListener('message', this.gameComplete.bind(this), false);
             }
         }
